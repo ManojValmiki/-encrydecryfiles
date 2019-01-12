b95948bbe93d1e3146928a2fec16437ec4b51026
ManojValmiki <46633193+ManojValmiki@users.noreply.github.com>
2019-01-13 01:27:37 +0530

    Update EncDec.py

--- EncDec.py
+++ EncDec.py
@@ -1,1 +1,39 @@
+import pyAesCrypt
+import os
+import getpass
+from os import stat, remove
+bufferSize = 64*1024
 
+def encrypt(infile,password):
+	with open(infile, 'rb') as fin:
+		with open(infile+'.enc','wb') as fout:
+				pyAesCrypt.encryptStream(fin,fout,password,bufferSize)
+				os.remove(infile)
+			
+def decrypt(infile,password):
+	with open(infile,'rb') as fin:
+		encfilesize = stat(infile).st_size
+		with open(infile[:-4],'wb') as fout:
+				try:
+					pyAesCrypt.decryptStream(fin,fout,password,bufferSize,encfilesize)
+					os.remove(infile)
+					print('Done')
+				except ValueError:
+					remove(infile[:-4])
+					print('Wrong password.....?')
+				
+def main():
+	choice = input('would u like to (E)encrypt or (D)decrypt: ')
+	if choice == 'E':
+		infile = input('enter file name: ')
+		password = getpass.getpass('password: ')
+		encrypt(infile,password)
+		print('Done')
+		
+	elif choice == 'D':
+		infile = input('enter file name: ')
+		password = getpass.getpass('password: ')
+		decrypt(infile,password)
+	
+if __name__ == '__main__':
+	main()
